c6ed23fa7638bb1f6b090504c5bb8bae92a0ba2d
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2015-11-28 02:55:23 +0000

    - Added key 'provides' to the Makefile.PL script.

--- lib/BankAccount/Validator/UK.pm
+++ lib/BankAccount/Validator/UK.pm
@@ -1,6 +1,6 @@
 package BankAccount::Validator::UK;
 
-$BankAccount::Validator::UK::VERSION   = '0.22';
+$BankAccount::Validator::UK::VERSION   = '0.23';
 $BankAccount::Validator::UK::AUTHORITY = 'cpan:MANWAR';
 
 =head1 NAME
@@ -9,7 +9,7 @@ BankAccount::Validator::UK - Interface to validate UK bank account.
 
 =head1 VERSION
 
-Version 0.22
+Version 0.23
 
 =cut
 
