25029d07e22fd90b78d459b5207cc4f6e9d8e463
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2016-11-29 12:21:16 +0000

    - Addition of new sorting codes. (v4.00) - Updated the valacdos.txt dated 9th Jan'2017.

--- lib/BankAccount/Validator/UK.pm
+++ lib/BankAccount/Validator/UK.pm
@@ -1,6 +1,6 @@
 package BankAccount::Validator::UK;
 
-$BankAccount::Validator::UK::VERSION   = '0.25';
+$BankAccount::Validator::UK::VERSION   = '0.26';
 $BankAccount::Validator::UK::AUTHORITY = 'cpan:MANWAR';
 
 =head1 NAME
@@ -9,7 +9,7 @@ BankAccount::Validator::UK - Interface to validate UK bank account.
 
 =head1 VERSION
 
-Version 0.25
+Version 0.26
 
 =cut
 
@@ -36,7 +36,7 @@ has sort_code  => (is => 'ro', default => sub { return BankAccount::Validator::U
 The module uses the algorithm provided by VOCALINK to validate the bank sort code
 and account number.  It is  done by modulus  checking  method as specified in the
 document which is available on their website L<VOCALINK|https://www.vocalink.com/customer-support/modulus-checking>
-It currently supports the L<document|https://www.vocalink.com/media/1911/vocalink-validating-account-numbers-v390.pdf> v3.90 dated 13th June'2016.
+It currently supports the L<document|https://www.vocalink.com/media/2101/vocalink-validating-account-numbers-v400.pdf> v4.00 dated 9th Jan'2017.
 
 Institutions covered by this document are below:
 
